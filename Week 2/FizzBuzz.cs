e11d19f7d130a95f0d3a5a3d6a7557422661d5c5
Avinash Sahani <avinash.sahani@astera.com>
2022-04-24 02:45:34 +0500

    Intial Test Case ( Faking Approach)

--- Week 2/FizzBuzz.cs
+++ Week 2/FizzBuzz.cs
@@ -1,9 +1,26 @@
 ﻿namespace TDD_In_C_Sharop_By_Engineer_Spock;
 using NUnit.Framework;
 
-
+//Fizz Buzz is simple program in which
+/*
+ * DIv BY 3 => "Fizz"
+ * Div By 5 => "Buzz"
+ * Div BY 3 and 5 => "Fizz Buzz"
+ * else => string.Empty
+ * 
+ */
 [TestFixture]
-public class FizzBuzz
+public class FizzBuzzTests
 {
+    [Test]
+    public void TestFizzBuzz()
+    {
+        Assert.AreEqual("Fizz",FizzBuzz(3));
+    }
     
+    private string FizzBuzz(int number)
+    {
+        return "Fizz";
+        
+    }
 }
