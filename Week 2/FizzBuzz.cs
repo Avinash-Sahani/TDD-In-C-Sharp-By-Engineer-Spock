32b3bc4f5fa8cfca9a46623a645dc12eb8253c1f
Avinash Sahani <avinash.sahani@astera.com>
2022-04-24 02:56:30 +0500

    Final Code Implementaion

--- Week 2/FizzBuzz.cs
+++ Week 2/FizzBuzz.cs
@@ -28,6 +28,8 @@ public class FizzBuzzTests
     
     private string FizzBuzz(int number)
     {
+        if (number % 3 == 0 && number % 5 == 0)
+            return "Fizz Buzz";
         if(number%3==0)
             return "Fizz";
         if (number % 5 == 0)
